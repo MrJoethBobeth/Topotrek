44fcec79ee0ae499818ccae15df94364f426088a
MrJoethBobeth <lohnesn1@gmail.com>
2025-07-10 14:07:52 -0400

    We're back

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import { MapView } from '@maplibre/maplibre-react-native';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as lightURL from './style.json';
 
 // Your Protomaps API Key
 const PROTOMAPS_API_KEY = '471215cca48e7f87';
 
 // Protomaps style URL. This style JSON uses a pmtiles source internally.
 // You can try other styles like 'dark', 'light', 'day', 'night'
-const protomapsStyleUrl = `https://api.protomaps.com/styles/v5/light/en.json?key=${PROTOMAPS_API_KEY}`;
+const protomapsStyleUrl = lightURL;
 
 
 const styles = StyleSheet.create({
